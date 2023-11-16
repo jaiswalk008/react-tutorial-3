ef66d814ae7792bd42aed80d23876a009903d06e
jaiswalk008 <jaiskaran008@gmail.com>
2023-11-17 01:56:19 +0530

    added timeout and handled error

--- src/App.js
+++ src/App.js
@@ -6,21 +6,40 @@ import './App.css';
 function App() {
   const [movies , setMovies] = useState([])
   const [loading , setLoading] = useState(false);
+  const [error , setError] = useState(null);
+  const [timeoutId, setTimeoutId] = useState(null); // create a state variable for timeoutId
   const fetchMovieHandler = async () =>{
     setLoading(true);
-    const response = await fetch('https://swapi.dev/api/films');
-    const data = await response.json();
+    // clearTimeout(timeoutId) 
+    console.log('retrying')
+    try {
+      const response = await fetch('https://swapi.dev/api/film');
+      
+      if(!response.ok){
+        throw new Error('Something went wrong....Retrying')
+      }  
+      const data = await response.json();
+      const transformedMovie = data.results.map(movie => {
+        return {
+          id:movie.episode_id,
+          title:movie.title,
+          openingText : movie.opening_crawl,
+          releaseDate: movie.release_date
+        };
+      })
+     setMovies(transformedMovie);
+    } catch (error) {
+      setError(error.message);
+      const id = setTimeout(fetchMovieHandler, 5000)
+      clearTimeout(timeoutId);
+      setTimeoutId(id);
+    }
     
-    const transformedMovie = data.results.map(movie => {
-      return {
-        id:movie.episode_id,
-        title:movie.title,
-        openingText : movie.opening_crawl,
-        releaseDate: movie.release_date
-      }
-    })
-    setMovies(transformedMovie);
-    
+  }
+  const cancelHandler = (e) =>{
+    setError(null);
+    setLoading(false);
+    clearTimeout(timeoutId) // clear the current timeout object
   }
 
   return (
@@ -29,7 +48,8 @@ function App() {
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        {loading &&( movies.length>0? <MoviesList movies={movies} /> : <h1>Loading....</h1>)}
+        {loading && !error && ( movies.length>0? <MoviesList movies={movies} /> : <h1>Loading....</h1>)}
+        {loading && error && <p>{error} <button onClick={cancelHandler}>Cancel retrying</button></p>}
       </section>
     </React.Fragment>
   );
